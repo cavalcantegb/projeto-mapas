3fd6fa3faba92fc7d6eabf40fbe8a160f7f4d0ef
Guilherme Bastos Cavalcante <cavalcante.guilherme91@gmail.com>
2020-08-31 23:40:10 -0300

    Reading and parsing .xlsx file

--- carga/urls.py
+++ carga/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('valeu',views.valeu, name='valeu'),
-    path('upload-spreadsheet', views.upload_spreadsheet, name='upload-spreadsheet')
+    path('upload_file', views.upload_file, name='upload_file')
 ]
+
+app_name='carga'
--- carga/views.py
+++ carga/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
-from .forms import NameForm
+from .forms import UploadFileForm
+from openpyxl import load_workbook
+from io import BytesIO
+from pprint import pprint
 
 def index(request):
     return render(request, 'index.html')
@@ -9,16 +11,19 @@ def index(request):
 def valeu(request):
     return render(request, 'valeu.html')
 
-def upload_spreadsheet(request):
+def upload_file(request):
     if request.method == 'POST':
-        return HttpResponseRedirect('valeu')
+        form = UploadFileForm(request.POST, request.FILES)
 
-def get_name(request):
-    if request.method == 'POST':
-        form = NameForm(request.POST)
-        print('form\n',flush=True)
         if form.is_valid():
-            return HttpResponseRedirect('/thanks/')
+            file_in_memory = request.FILES['file'].read()
+            wb = load_workbook(filename=BytesIO(file_in_memory))
+            wob = load_workbook(filename=request.FILES['file'].file)
+            ws = wb.active
+            for row in ws.iter_rows(min_row=6, max_col=5, max_row=10):
+                print(row)
+
+            return HttpResponseRedirect('/carga/valeu')
     else:
-        form = NameForm()
-    return render(request, 'name.html', {'form': form})
+        form = UploadFileForm()
+    return render(request, 'index.html', {'form':form})
