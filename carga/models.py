405c9ebd98868082148a21243627b131c8dee961
Guilherme Bastos Cavalcante <cavalcante.guilherme91@gmail.com>
2020-09-02 15:11:50 -0300

    Created project model. Updated Dockerfile and docker-compose.yml to run migrations.

--- carga/models.py
+++ carga/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    id_project = models.UniqueConstraint
+    name = models.CharField(max_length=1000)
+    description_short = models.CharField(max_length=500)
+    description_long = models.CharField
+    registration_from = models.DateTimeField()
+    registration_to = models.DateTimeField()
+    created_at = models.DateTimeField()
+    parent_entity = models.CharField(max_length=500)
+    published_by = models.CharField(max_length=500)
+    project_budget = models.DecimalField(max_digits=6, decimal_places=2)
+    project_goal = models.CharField
+    target_audience = models.CharField
+    areas = models.CharField
+    tags = models.CharField
+    verified_account = models.BooleanField(default=False)
+    project_type = models.CharField
+    project_website = models.URLField
+    project_facebook = models.URLField
+    project_twitter = models.CharField
+    project_google_plus = models.CharField
+    project_instagram = models.CharField
+    stamps = models.CharField
+    opportunity_tab_name = models.CharField
+    use_opportunity_tab = models.BooleanField(default=False)
--- carga/views.py
+++ carga/views.py
@@ -5,6 +5,33 @@ from openpyxl import load_workbook
 from io import BytesIO
 from pprint import pprint
 
+projects_dict = {'A' : 'id_project',
+              'B' : 'name',
+              'C' : 'description_short',
+              'D' : 'description_long',
+              'E' : 'last_update',
+              'F' : 'registration_from',
+              'G' : 'registration_to',
+              'H' : 'created_at',
+              'I' : 'parent_entity',
+              'J' : 'published_by',
+              'K' : 'project_budget',
+              'L' : 'project_goal',
+              'M' : 'target_audience',
+              'O' : 'areas',
+              'P' : 'tags',
+              'Q' : 'verified_account',
+              'R' : 'project_type',
+              'S' : 'project_website',
+              'T' : 'project_facebook',
+              'U' : 'project_twitter',
+              'V' : 'project_googleplus',
+              'W' : 'project_instagram',
+              'X' : 'stamps',
+              'Y' : 'opportunity_tab_name',
+              'Z' : 'uses_opportunity_tab'
+              }
+
 def index(request):
     return render(request, 'index.html')
 
@@ -21,7 +48,8 @@ def upload_file(request):
             wob = load_workbook(filename=request.FILES['file'].file)
             ws = wb.active
             for row in ws.iter_rows(min_row=6, max_col=5, max_row=10):
-                print(row)
+                for cell in row:
+                    print(cell.value)
 
             return HttpResponseRedirect('/carga/valeu')
     else:
