9c132750170ae3ac971509333a7666f3ff3a8865
Guilherme Bastos Cavalcante <cavalcante.guilherme91@gmail.com>
2020-09-05 20:54:30 -0300

    Adding metabase to the project, metabase will need t be put with a volume on next updates.

--- carga/models.py
+++ carga/models.py
@@ -3,26 +3,30 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     id_project = models.UniqueConstraint
-    name = models.CharField(max_length=1000)
-    description_short = models.CharField(max_length=500)
-    description_long = models.CharField
-    registration_from = models.DateTimeField()
-    registration_to = models.DateTimeField()
-    created_at = models.DateTimeField()
-    parent_entity = models.CharField(max_length=500)
-    published_by = models.CharField(max_length=500)
-    project_budget = models.DecimalField(max_digits=6, decimal_places=2)
-    project_goal = models.CharField
-    target_audience = models.CharField
-    areas = models.CharField
-    tags = models.CharField
+    name = models.CharField(max_length=1000, default="", blank=True, null=True)
+    description_short = models.CharField(max_length=1000, default="", blank=True, null=True)
+    description_long = models.CharField(max_length=1000, default="", blank=True, null=True)
+    last_update = models.DateTimeField(null=True, blank=True)
+    registration_from = models.DateTimeField(null=True, blank=True)
+    registration_to = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(null=True, blank=True)
+    parent_entity = models.CharField(max_length=1000, default="", blank=True, null=True)
+    published_by = models.CharField(max_length=1000, default="", blank=True, null=True)
+    project_budget = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    project_goal = models.CharField(max_length=1000, default="", blank=True, null=True)
+    target_audience = models.CharField(max_length=1000, default="", blank=True, null=True)
+    areas = models.CharField(max_length=1000, default="", blank=True, null=True)
+    tags = models.CharField(max_length=1000, default="", blank=True, null=True)
     verified_account = models.BooleanField(default=False)
-    project_type = models.CharField
-    project_website = models.URLField
-    project_facebook = models.URLField
-    project_twitter = models.CharField
-    project_google_plus = models.CharField
-    project_instagram = models.CharField
-    stamps = models.CharField
-    opportunity_tab_name = models.CharField
+    project_type = models.CharField(max_length=1000, default="", blank=True, null=True)
+    project_website = models.URLField(max_length=1000, default="", blank=True, null=True)
+    project_facebook = models.URLField(max_length=1000, default="", blank=True, null=True)
+    project_twitter = models.CharField(max_length=1000, default="", blank=True, null=True)
+    project_google_plus = models.CharField(max_length=1000, default="", blank=True, null=True)
+    project_instagram = models.CharField(max_length=1000, default="", blank=True, null=True)
+    stamps = models.CharField(max_length=1000, default="", blank=True, null=True)
+    opportunity_tab_name = models.CharField(max_length=1000, default="", blank=True, null=True)
     use_opportunity_tab = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{} {} {}".format(self.id_project, self.name, self.description_short)
--- carga/views.py
+++ carga/views.py
@@ -4,39 +4,17 @@ from .forms import UploadFileForm
 from openpyxl import load_workbook
 from io import BytesIO
 from pprint import pprint
-
-projects_dict = {'A' : 'id_project',
-              'B' : 'name',
-              'C' : 'description_short',
-              'D' : 'description_long',
-              'E' : 'last_update',
-              'F' : 'registration_from',
-              'G' : 'registration_to',
-              'H' : 'created_at',
-              'I' : 'parent_entity',
-              'J' : 'published_by',
-              'K' : 'project_budget',
-              'L' : 'project_goal',
-              'M' : 'target_audience',
-              'O' : 'areas',
-              'P' : 'tags',
-              'Q' : 'verified_account',
-              'R' : 'project_type',
-              'S' : 'project_website',
-              'T' : 'project_facebook',
-              'U' : 'project_twitter',
-              'V' : 'project_googleplus',
-              'W' : 'project_instagram',
-              'X' : 'stamps',
-              'Y' : 'opportunity_tab_name',
-              'Z' : 'uses_opportunity_tab'
-              }
+import datetime
+from .models import Project
+from .dictionaries import projects_dict_letter
 
 def index(request):
     return render(request, 'index.html')
 
+
 def valeu(request):
     return render(request, 'valeu.html')
+
 
 def upload_file(request):
     if request.method == 'POST':
@@ -47,11 +25,73 @@ def upload_file(request):
             wb = load_workbook(filename=BytesIO(file_in_memory))
             wob = load_workbook(filename=request.FILES['file'].file)
             ws = wb.active
-            for row in ws.iter_rows(min_row=6, max_col=5, max_row=10):
+            for row in ws.iter_rows(min_row=6, max_col=26, max_row=ws.max_row):
+                project = Project()
                 for cell in row:
-                    print(cell.value)
+                    check_column = cell.column in projects_dict_letter
+                    if check_column:
+                        if cell.column == "A":
+                            project.id_project = cell.value
+                        elif cell.column == "B":
+                            project.name = cell.value
+                        elif cell.column == "C":
+                            project.description_short = cell.value
+                        elif cell.column == "D":
+                            project.description_long = cell.value
+                        elif cell.column == "E":
+                            if cell.value != None:
+                                project.last_update = cell.value
+                        elif cell.column == "F":
+                            if cell.value != None:
+                                project.registration_from = cell.value
+                        elif cell.column == "G":
+                            if cell.value != None:
+                                project.registration_to = cell.value
+                        elif cell.column == "H":
+                            if cell.value != None:
+                                project.created_at = cell.value
+                        elif cell.column == "I":
+                            project.parent_entity = cell.value
+                        elif cell.column == "J":
+                            project.published_by = cell.value
+                        elif cell.column == "K":
+                            project.project_budget = cell.value
+                        elif cell.column == "L":
+                            project.project_goal = cell.value
+                        elif cell.column == "M":
+                            project.target_audience = cell.value
+                        elif cell.column == "O":
+                            project.areas = cell.value
+                        elif cell.column == "P":
+                            project.tags = cell.value
+                        elif cell.column == "Q":
+                            project.verified_account = cell.value
+                        elif cell.column == "R":
+                            project.project_type = cell.value
+                        elif cell.column == "S":
+                            project.project_website = cell.value
+                        elif cell.column == "T":
+                            project.project_facebook = cell.value
+                        elif cell.column == "U":
+                            project.project_twitter = cell.value
+                        elif cell.column == "V":
+                            project.project_google_plus = cell.value
+                        elif cell.column == "W":
+                            project.project_instagram = cell.value
+                        elif cell.column == "X":
+                            project.stamps = cell.value
+                        elif cell.column == "Y":
+                            project.opportunity_tab_name = cell.value
+                        elif cell.column == "Z":
+                            project.uses_opportunity_tab = cell.value
+                pprint(project.__str__())
+                try:
+                    project.validate_unique()
+                    project.save()
+                except:
+                    None
 
             return HttpResponseRedirect('/carga/valeu')
     else:
         form = UploadFileForm()
-    return render(request, 'index.html', {'form':form})
+    return render(request, 'index.html', {'form': form})
