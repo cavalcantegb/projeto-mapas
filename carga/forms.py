1789c3dbd1568a5adc02ccb6cd7d81e47da79e6f
Guilherme Bastos Cavalcante <cavalcante.guilherme91@gmail.com>
2020-09-07 18:48:15 -0300

    Adjusting forms

--- carga/forms.py
+++ carga/forms.py
@@ -4,4 +4,5 @@ from .types import table_type
 
 class UploadFileForm(forms.Form):
     file = forms.FileField()
-    table = forms.ChoiceField(choices=table_type, widget=forms.RadioSelect)
+    table = forms.ChoiceField(choices=table_type, widget=forms.RadioSelect, initial=table_type[0], required=True)
+
